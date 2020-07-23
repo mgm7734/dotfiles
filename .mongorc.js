81df07c01a5edb47493861d9e15f8c07508c5f74
Mark Mendel <mmendel@meinergy.com>
2020-07-23 01:18:22 -0500

    lazy queries

--- .mongorc.js
+++ .mongorc.js
@@ -1,114 +1,86 @@
-// interpreterVersion()
-
-function get_path() {
-    var result = arguments.shift
-
+/** @return {DBCollection} dataset collection for give project- and dataset-code */
+function data(projectCode, datasetCode='pilrhealth:mobile:survey_data', kind='data') {
+    const proj = db.project.findOne({$or: [{ code: projectCode },
+                                           { name: projectCode }]})
+          || die('no project:', projectCode);
+    const dataset = db.dataset.findOne({ code: datasetCode, project: proj._id})
+          || die('no dataset:', datasetCode);
+    return db[dataset._id + ':' + kind];
 }
 
-
-function purgeUser(uid) {
-    printjson(  db.authorization.deleteMany({uid: uid}) )
-    printjson( db.userRole.deleteMany({user: uid}) )
-    db.user.remove({_id: uid})
-}
-
-/*
- * Index nested object in an array or object.n
- * Path can be '.' separated key string or array of keys. For arrays, path can be a number.
- */
-function getPath(obj, path) {
-    if (path.length == 0)
-        return obj
-    if (typeof obj != 'object')
-        return undefined
-    if (typeof path == 'string')
-        path = path.split('.')
-    return getPath(obj[path[0]], path.slice(1))
-}
-
-function getter() {
-    var path
-    if (arguments.length == 1)
-        path = arguments[0]
-1    else
-        path = [].slice.call(arguments)
-    return function(obj) {
-        return getPath(obj, path)
-    }
-}
-
-function colls4proj(code) {
-    var proj = db.project.findOne({code:code})
-    return db.getCollectionNames().filter(function(nm) { return nm.endsWith(proj._id) })
-}
-
-//// Misc array utilities
-function sum(a) { return a.reduce((a,x) => a+x, 0) }
-function min(a) { return a.reduce((acc, v) => v < acc ? v : a) }
-function max(a) { return a.reduce((acc, v) => v > acc ? v : a) }
-
-Array.prototype.limit = function(n) {
-    let r = []
-    for (let i = 0; i < n; ++i) {
-        r[i] = this[i]
-    }
-    return r
-}
-
-Object.prototype.tap = function(f) {f(this); return this}
-
-/*
-Function Set(hashFunction) {
-	this._hashFunction = hashFunction || JSON.stringify;
-	this._values = {};
-	this._size = 0;
-}
-
-Set.prototype = {
-	add: function add(value) {
-		if (!this.contains(value)) {
-			this._values[this._hashFunction(value)] = value;
-			this._size++;
-		}
-	},
-
-	remove: function remove(value) {
-		if (this.contains(value)) {
-			delete this._values[this._hashFunction(value)];
-			this._size--;
-		}
-	},
-
-	contains: function contains(value) {
-		return typeof this._values[this._hashFunction(value)] !== "undefined";
-	},
-
-	size: function size() {
-		return this._size;
-	},
-
-	forEach: function each(iteratorFunction, thisObj) {
-		for (var value in this._values) {
-			iteratorFunction.call(thisObj, this._values[value]);
-		}
-	}
-}
-*/
-
-///////// PiLR
-
-function addProjectOwner(projCode, username) {
+function addProjectOwner(projCode, username = 'mmendel') {
     var projId = db.project.findOne({code: projCode})._id
     var newOwnerId=db.user.findOne({username: username})._id
-    if (db.authorization.count({instanceId: projId, permission: {$exists: 0}, uid: newOwnerId}) > 0) {
-        printjson('already an owner')
-        return
-    }
+    0 == db.authorization.count({instanceId: projId, permission: {$exists: 0}, uid: newOwnerId})
+        || die(username, 'is already an owner');
+
     var someOwnerAuth = db.authorization.findOne({instanceId: projId, permission: {$exists: 0}})
-    printjson(someOwnerAuth.uid)
-    return db.authorization.find({instanceId: projId, uid: someOwnerAuth.uid}).forEach(auth => {
-        auth.uid = newOwnerId
-        delete auth._id
-        db.authorization.insert(auth)
+    printjson(someOwnerAuth.uid);
+    db.authorization.find({instanceId: projId, uid: someOwnerAuth.uid}).forEach(auth => {
+        auth.uid = newOwnerId;
+        delete auth._id;
+        db.authorization.insert(auth);
+    });
+}
+
+function die(...strs) {
+    throw strs.join(' ');
+}
+
+function unique(array) {
+    return array.filter((el, ix) => array.indexOf(el) == ix)
+}
+
+DBCollection.prototype.get = function(id) { return this.findOne({_id: id}) }
+
+/**
+ * Chainable queries.
+ *  Example:
+ *    q('project', {code: /^r21/}).child('instrument').child('instrumentConfig')
+ *    .child('emaOtsCardstack', 'configuration').prop('code')
+q('project', {code: /^r21/}).child('instrument').find()
+   */
+function LazyQuery(collectionName, opts={}) {
+    const query = opts.query || {};
+    const stack = opts.stack || [];
+    Object.assign(this, {
+        collectionName, query,
+        collection: db[collectionName],
+
+        find(query = {}) {
+            return this.collection.find({$and: [this.query, query]})
+        },
+        count() {  return this.collection.count(this.query) },
+        id() { return this.prop('_id')},
+        code() { return this.prop('code)') },
+        at(ix) { return this.find().skip(ix).next() },
+        prop(p) { return this.find().map(d=>d[p]) },
+        id() { return this.prop('_id' )},
+        code() { return this.prop('code')},
+
+        /** Refine the current query */
+        where(q) {
+            return new LazyQuery(this.collectionName,
+                                 Object.assign({}, opts, {query: { $and: [ this.query, q ] }}) );
+        },
+
+        /** Chain a subquery */
+        q(cn, q) {
+            return new LazyQuery(cn, { query: q, stack: [ this, ...stack ] })
+        },
+        child(childName, prop = null) {
+            if (!prop) prop = this.collectionName;
+            let parentIds = this.id();
+            //return new LazyQuery(childName, {query: { [prop] : { $in: parentIds } }})
+            return this.q(childName, { [prop] : { $in: parentIds } });
+        },
+        parent(parentName, prop = null) {
+            if (!prop) prop = parentName;
+            let parentIds = this.prop(prop);
+            return new LazyQuery(parentName, {query: {_id: {$in: parentIds}}});
+        }
     })
 }
+function q(collName, query) { return new LazyQuery(collName, {query}) }
+
+q('project', {code: 'r21testproject'}).child('instrument').child('instrumentConfig').child('emaOtsCardstack', 'configuration').count()
